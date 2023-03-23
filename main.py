5b53d02e2b94f3dbbb8948331cc34cb41880ac88
Wolfgang <alex.dinkelmeyer@gmail.com>
2023-03-23 17:22:16 +0000

    setup: added main.py template code

---
+++ main.py
@@ -0,0 +1,13 @@
+"""The AI system"""
+# add imports as needed examples:
+# from card import Card
+# from player import Player
+# from game_state import GameState
+# from gameplay import play_card, attack, draw_card
+
+def main():
+    """Main program logic"""
+    print("Hello MTG")
+
+if __name__ == "__main__":
+    main()
