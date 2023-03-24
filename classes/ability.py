9ca647e953a8a09bf264fab3513036855a246330
Wolfgang <alex.dinkelmeyer@gmail.com>
2023-03-24 00:39:21 +0000

    class: add ability

---
+++ classes/ability.py
@@ -0,0 +1,6 @@
+class Ability:
+    def __init__(self, name, ability_type, effect, cost):
+        self.name = name
+        self.ability_type = ability_type
+        self.effect = effect
+        self.cost = cost
