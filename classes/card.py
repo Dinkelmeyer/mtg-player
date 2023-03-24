dd26c379ba86a22adbc9de1dcff92b017b4b5caf
Wolfgang <alex.dinkelmeyer@gmail.com>
2023-03-24 00:26:55 +0000

    class: add card

---
+++ classes/card.py
@@ -0,0 +1,6 @@
+class Card:
+    def __init__(self, name, mana_cost, card_type, abilities):
+        self.name = name
+        self.mana_cost = mana_cost
+        self.card_type = card_type
+        self.abilities = abilities
