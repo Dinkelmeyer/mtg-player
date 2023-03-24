98ae3ee3900b6799cded10823ae905f00b91730d
Wolfgang <alex.dinkelmeyer@gmail.com>
2023-03-24 00:31:45 +0000

    class: add Player

---
+++ classes/player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, life_total, hand, library, graveyard, exile):
+        self.life_total = life_total
+        self.hand = hand
+        self.library = library
+        self.graveyard = graveyard
+        self.exile = exile
