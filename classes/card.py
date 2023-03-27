eb7ec848950366a40140082c61b62fe9850916bf
Wolfgang <alex.dinkelmeyer@gmail.com>
2023-03-27 19:30:22 +0000

    🔨 refactor: Add card subtypes to Card class constructor

--- classes/card.py
+++ classes/card.py
@@ -1,6 +1,8 @@
 class Card:
-    def __init__(self, name, mana_cost, card_type, abilities):
+    def __init__(self, name, mana_cost, card_type, card_subtypes, abilities):
         self.name = name
         self.mana_cost = mana_cost
         self.card_type = card_type
+        self.card_subtypes = card_subtypes
         self.abilities = abilities
+        
